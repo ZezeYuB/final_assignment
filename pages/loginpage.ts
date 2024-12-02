5e6dcecd833900cc58df2e652869d1b2cd96f466
ZezeYub <a.zaya.boldbaatar@gmail.com>
2024-12-02 21:48:36 +0100

    added secret env

--- pages/loginpage.ts
+++ pages/loginpage.ts
@@ -3,7 +3,7 @@ import { Locator, Page } from "@playwright/test";
 export class LoginPage {
     readonly page: Page;
     readonly usernameInput: Locator;
-    readonly password: Locator;
+    readonly passwordInput: Locator;
     readonly roleSelector: Locator;
     readonly submitButton: Locator;
     readonly errorMessage: Locator;
@@ -11,7 +11,7 @@ export class LoginPage {
     constructor (page: Page) {
         this.page = page;
         this.usernameInput = page.getByLabel("Username");
-        this.password = page.getByLabel("Password");
+        this.passwordInput = page.getByLabel("Password");
         this.roleSelector = page.getByLabel("Select Role");
         this.submitButton = page.getByRole('button', {name: "Login"});
         this.errorMessage = page.getByTestId("error-message");
@@ -20,12 +20,14 @@ export class LoginPage {
     
     }
 
-    async login(username: string, password: string, userType: string) {
-        await this.usernameInput.fill(username);
-        await this.password.fill(password);
-        await this.roleSelector.selectOption(userType);
-        await this.submitButton.click();
-        
+async login(username: string, password: string, userType: string){
+    await this.usernameInput.fill(username)
+    await this.passwordInput.fill(password)
+    await this.roleSelector.selectOption(userType)
+    await this.submitButton.click()
+    
 
-    }
+}        
+
+    
 }
--- tests/order.spec.ts
+++ tests/order.spec.ts
@@ -2,30 +2,33 @@ import test, { expect } from "@playwright/test"
 import {LoginPage} from "../pages/loginpage"
 import {StorePage} from "../pages/storepage"
 import exp from "constants"
+import { PurchasePage } from "../pages/purchasepage"
 
-let password: string;
-test('Login with Markus', async({page}) =>{
+
+test.skip('Login with Markus', async({page}) =>{
     const loginPage = new LoginPage(page)
     const storePage = new StorePage(page)
-
-    if (process.env.PASSWORD !== undefined){
-        password = process.env.PASSWORD
-
-    }
-
     await page.goto("https://hoff.is/login")
 
-    await loginPage.login("Markus", password,"consumer")
+    await loginPage.login("Markus", "sup3rs3cr3t","consumer")
     const username = await storePage.usernameText.textContent()
     const header = await storePage.header.textContent()
     expect(header).toBe("Store")
 })
 
-test('Login with incorrect details', async({page}) =>{
+test.skip('Login with incorrect details', async({page}) =>{
     const loginPage = new LoginPage(page)
     await page.goto("https://hoff.is/login")
 
     await loginPage.login("Markus", "sup3rs3cr2t","consumer")
     const errorMessage = await loginPage.errorMessage.textContent()
     expect(errorMessage).toBe("Incorrect password")
+})
+
+test('Place a purchase', async({page}) => {
+    const purchasePage = new PurchasePage(page);
+    await page.goto("https://hoff.is/store2/?username=markus&role=business")
+
+    await purchasePage.buyOnePurchase("3", "3", "AZB", "Test123")
+    
 })
