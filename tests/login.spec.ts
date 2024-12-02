d4e6c9a9eb8358d4ea52365a58cdbe8f18689b36
ZezeYub <a.zaya.boldbaatar@gmail.com>
2024-12-02 22:23:28 +0100

    testing the password

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,19 +1,27 @@
 import test, { expect } from "@playwright/test"
 import {LoginPage} from "../pages/loginpage"
 import {StorePage} from "../pages/storepage"
-import exp from "constants"
+import dotenv from 'dotenv'
 
-let password: string;
+//let password: string;
 test('Login with Markus', async({page}) =>{
     const loginPage = new LoginPage(page)
     const storePage = new StorePage(page)
+    dotenv.config(); // Loads variables from .env file into process.env
 
-    if (process.env.PASSWORD !== undefined){
-        password = process.env.PASSWORD
-
+    const password = process.env.PASSWORD; // Retrieve the password from .env
+    if (!password) {
+        throw new Error('Environment variable PASSWORD is not set');
     }
 
+
+    /*if (process.env.PASSWORD !== undefined){
+        password = process.env.PASSWORD
+
+    }*/
+
     await page.goto("https://hoff.is/login")
+    console.log("The value is: " + process.env.PASSWORD);
 
     await loginPage.login("Markus", password,"consumer")
     const username = await storePage.usernameText.textContent()
